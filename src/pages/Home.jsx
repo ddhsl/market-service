bcaa1fc9827578d29c34aa064b020c5409f54424
ddhsl <dnjs9199@naver.com>
2025-01-04 18:14:37 +0900

    💄 Home 페이지 추가

---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "../components/Header";
+import Browsing from "../components/Browsing";
+import Main from "../components/Main";
+import Footer from "../components/Footer";
+// import styled from "styled-components";
+
+const App = () => {
+  return (
+    <>
+      <Header></Header>
+      <Browsing></Browsing>
+      <Main></Main>
+      <Footer></Footer>
+    </>
+  );
+};
+
+export default App;
