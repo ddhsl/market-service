9e0a7c2e7226a31eba220b50cbf53c74f15de3e0
ddhsl <dnjs9199@naver.com>
2025-01-05 23:54:29 +0900

    💄 JoinForm 컴포넌트 생성

---
+++ src/pages/join/component/JoinForm.jsx
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+import AccountTaps from "../../../components/AccountTaps";
+
+export default function JoinForm() {
+  return <AccountTaps formType="join"></AccountTaps>;
+}
