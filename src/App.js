fd8ddc34c76dc10bf2e336d330ea78eef3a3bc73
ddhsl <dnjs9199@naver.com>
2025-01-05 23:57:28 +0900

    ✨ Login, Join 라우터 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import GlobalStyle from "./styles/GlobalStyle";
 import Home from "./pages/Home.jsx";
+import Login from "./pages/login/Login.jsx";
+import Join from "./pages/join/Join.jsx";
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <GlobalStyle />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/join" element={<Join />} />
       </Routes>
     </BrowserRouter>
   );
