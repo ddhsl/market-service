6aa3de7e2b7fb9a537d7e1c98d2d1646167fdd29
ddhsl <dnjs9199@naver.com>
2025-01-06 20:52:45 +0900

    🎨 Alert 컴포넌트 분리 및 LoginInput 스타일 추가

--- src/pages/login/component/LoginForm.jsx
+++ src/pages/login/component/LoginForm.jsx
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 import Input from "../../../components/Input";
 import InputField from "../../../components/InputField";
+import Button from "../../../components/Button";
+import AlertMsg from "../../../components/AlertMsg";
 
 export default function LoginForm() {
   return (
@@ -8,45 +10,23 @@ export default function LoginForm() {
       <label htmlFor="id" className="sr-only">
         아이디를 입력하세요
       </label>
-      <Input
-        type="text"
-        name="id"
-        placeholder="아이디"
-        border="none"
-        borderBottom="1px solid #c4c4c4"
-      />
+      <LoginInput type="text" name="id" placeholder="아이디" />
       <label htmlFor="pw" className="sr-only">
         비밀번호를 입력하세요
       </label>
-      <Input
-        type="password"
-        name="pw"
-        placeholder="비밀번호"
-        border="none"
-        borderBottom="1px solid #c4c4c4"
-      />
+      <LoginInput type="password" name="pw" placeholder="비밀번호" />
       <AlertMsg>아이디를 입력해 주세요.</AlertMsg>
-      <LoginBtn>로그인</LoginBtn>
+      <Button width="480px" height="60px" fontSize="18px" marginTop="28px">
+        로그인
+      </Button>
     </InputField>
   );
 }
 
-const AlertMsg = styled.p`
-  color: #eb5757;
-  font-weight: 500;
-  margin-top: 26px;
-  margin-left: 36px;
-  display: none;
-`;
-
-const LoginBtn = styled.button`
-  width: 480px;
+const LoginInput = styled(Input)`
+  border: none;
+  border-bottom: 1px solid #c4c4c4;
+  border-radius: 0;
   height: 60px;
-  background-color: var(--main-color);
-  color: #fff;
-  font-size: 18px;
-  font-weight: 700;
-  border-radius: 5px;
-  margin: 36px 35px;
-  text-align: center;
+  padding-left: 0;
 `;
