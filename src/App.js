d3bd258ff1a7405ce689760fe4ba03269d9ad6c4
ddhsl <dnjs9199@naver.com>
2025-01-22 23:30:32 +0900

    ✨ product details 페이지 라우터 추가

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import GlobalStyle from "./styles/GlobalStyle";
 import Home from "./pages/Home.jsx";
 import Login from "./pages/login/Login.jsx";
 import Join from "./pages/join/Join.jsx";
+import ProductDetails from "./pages/ProductDetails/ProductDetails.jsx";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/join" element={<Join />} />
+        <Route path="/product-details/:id" element={<ProductDetails />} />
       </Routes>
     </BrowserRouter>
   );
