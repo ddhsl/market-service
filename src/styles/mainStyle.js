e0a6f461ab9188f85fc2f42ff7f5fe62e7219241
ddhsl <dnjs9199@naver.com>
2025-02-06 20:45:17 +0900

    💄 Product Card 공통 스타일 추가

--- src/styles/mainStyle.js
+++ src/styles/mainStyle.js
@@ -11,25 +11,55 @@ export const Main = styled.main`
 
 export const ProductCard = styled.article``;
 
+//product card 공통 스타일 정의
+
+const variants = {
+  main: {
+    productImg: {
+      border: "1px solid #c4c4c4",
+      borderRadius: "10px",
+      width: "370px",
+      height: "370px",
+      marginBottom: "16px",
+    },
+    storeName: { color: "var(--sub-color)", marginBottom: "10px" },
+    productName: { fontSize: "18px", marginBottom: "10px" },
+    price: { fontSize: "24px", fontWeight: "bold" },
+  },
+  detail: {
+    productImg: { width: "320px", height: "250px" },
+    storeName: {
+      color: "var(--sub-color)",
+      fontSize: "18px",
+      marginBottom: "16px",
+    },
+    productName: { fontSize: "36px", marginBottom: "20px" },
+    price: { fontSize: "36px", fontWeight: "bold" },
+  },
+};
+
 export const ProductImg = styled.img`
-  border: 1px solid #c4c4c4;
-  border-radius: 10px;
-  width: 370px;
-  height: 370px;
-  margin-bottom: 16px;
+  border: ${({ variant }) => variants[variant]?.productImg.border};
+  border-radius: ${({ variant }) => variants[variant]?.productImg.borderRadius};
+  width: ${({ variant }) => variants[variant]?.productImg.width};
+  height: ${({ variant }) => variants[variant]?.productImg.height};
+  margin-bottom: ${({ variant }) => variants[variant]?.productImg.marginBottom};
 `;
 
 export const StoreName = styled.p`
   color: var(--sub-color);
-  margin-bottom: 10px;
+  font-size: ${({ variant }) => variants[variant]?.storeName.fontSize};
+  font-weight: ${({ variant }) => variants[variant]?.storeName.fontWeight};
+  margin-bottom: ${({ variant }) => variants[variant]?.storeName.marginBottom};
 `;
 
 export const ProductName = styled.h3`
-  font-size: 18px;
-  margin-bottom: 10px;
+  font-size: ${({ variant }) => variants[variant]?.productName.fontSize};
+  margin-bottom: ${({ variant }) =>
+    variants[variant]?.productName.marginBottom};
 `;
 
 export const Price = styled.span`
-  font-size: 24px;
-  font-weight: bold;
+  font-size: ${({ variant }) => variants[variant]?.price.fontSize};
+  font-weight: ${({ variant }) => variants[variant]?.price.fontWeight};
 `;
