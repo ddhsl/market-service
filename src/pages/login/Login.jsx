d30d40fca82317692e9636135021524b994bebcd
ddhsl <dnjs9199@naver.com>
2025-02-06 20:46:54 +0900

    ✨ 로그인 타입 별 tab 클릭 기능 추가

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -4,8 +4,15 @@ import AccountTaps from "../../components/AccountTaps";
 import LoginForm from "./component/LoginForm";
 import AccountLinks from "./component/AccountLinks";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export default function Login() {
+  const [selectedTab, setSelectedTab] = useState("buyer"); // selectedTab 상태 추가
+
+  const handleTab = (tab) => {
+    setSelectedTab(tab); // Tab 클릭 시 selectedTab 업데이트
+  };
+
   return (
     <LoginWrap>
       <h1>
@@ -13,7 +20,11 @@ export default function Login() {
           <LogoImg src={logo} alt="HODU" />
         </Link>
       </h1>
-      <AccountTaps formType="login" />
+      <AccountTaps
+        formType="login"
+        handleTab={handleTab}
+        selectedTab={selectedTab}
+      />
       <LoginForm />
       <AccountLinks />
     </LoginWrap>
