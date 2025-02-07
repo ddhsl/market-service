a873c802fa1f2d339dedcd847ad4590efe57c64a
ddhsl <dnjs9199@naver.com>
2025-02-08 03:34:22 +0900

    💄 PhoneInput 컴포넌트 생성

---
+++ src/pages/paymentPage/component/PhoneInput.jsx
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+import { StyledShippingInput } from "./ShippingInput";
+
+export default function PhoneInput({ idPrefix, namePrefix }) {
+  return (
+    <div style={{ display: "flex", gap: "10px", alignItems: "center" }}>
+      <StyledShippingInput
+        id={`${idPrefix}-first`}
+        name={`${namePrefix}First`}
+        width="80px"
+      />
+      <PhoneSeparator>-</PhoneSeparator>
+      <StyledShippingInput
+        id={`${idPrefix}-middle`}
+        name={`${namePrefix}Middle`}
+        width="100px"
+      />
+      <PhoneSeparator>-</PhoneSeparator>
+      <StyledShippingInput
+        id={`${idPrefix}-last`}
+        name={`${namePrefix}Last`}
+        width="100px"
+      />
+    </div>
+  );
+}
+
+const PhoneSeparator = styled.span`
+  padding: 18px 10px;
+`;
