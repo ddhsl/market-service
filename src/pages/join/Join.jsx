610ad191c5e8324199df3a54af5f75502a874d63
ddhsl <dnjs9199@naver.com>
2025-03-11 02:26:42 +0900

    🎨 동의 및 가입하기 컴포넌트 분리

--- src/pages/join/Join.jsx
+++ src/pages/join/Join.jsx
@@ -4,16 +4,14 @@ import logo from "../../assets/Logo-hodu.png";
 import { Link } from "react-router-dom";
 import AccountTaps from "../../components/AccountTaps";
 import JoinForm from "./component/JoinForm";
-import Button from "../../components/Button";
-import checkImg from "../../assets/check-box.svg";
 
 export default function Join() {
   const [selectedTab, setSelectedTab] = useState("buyer"); // selectedTab 상태 추가
 
   const handleTab = (tab) => {
-    //(tab) 함수의 매개변수
-    setSelectedTab(tab); // Tab 클릭 시 selectedTab 업데이트할 수 있도록 함수를 생성함
-  }; //클릭이벤트에서 setSelectedTab="buyer"과 같이 직접 상태 변경하는 것보다 함수 생성이 유지보수 & 가독성 향상
+    setSelectedTab(tab);
+    console.log(tab);
+  };
 
   return (
     <JoinWrap>
@@ -27,16 +25,7 @@ export default function Join() {
         handleTab={handleTab}
         selectedTab={selectedTab}
       />
-      <JoinForm formType="join" selectedTab={selectedTab} />{" "}
-      {/* selectedTab 전달 */}
-      <CheckImg src={checkImg} alt="동의하기" />
-      <p>
-        호두샵의 <span>이용약관</span> 및 <span>개인정보처리방침</span>에 대한
-        내용을 확인하였고 동의합니다.
-      </p>
-      <Button width="480px" height="60px" fontSize="18px">
-        가입하기
-      </Button>
+      <JoinForm formType="join" selectedTab={selectedTab} />
     </JoinWrap>
   );
 }
@@ -67,11 +56,4 @@ const LogoImg = styled.img`
   width: 238px;
   height: 74px;
   margin-top: 70px;
-`;
-
-const CheckImg = styled.img`
-  margin-right: 10px;
-  position: relative;
-  top: 28px;
-  right: 228px;
 `;
