ac42b86953381b9d0c5526a3002c2196f332ed4e
ddhsl <dnjs9199@naver.com>
2025-03-22 05:00:10 +0900

    🎨 공통 Button 컴포넌트 props 값 추가

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,13 +5,16 @@ export default function Button(props) {
 }
 
 const StyledButton = styled.button`
-  width: ${(props) => props.width};
-  height: ${(props) => props.height};
-  background-color: var(--main-color);
-  color: #fff;
+  width: ${({ width }) => width || "auto"};
+  height: ${({ height }) => height || "auto"};
+  background-color: ${({ backgroundColor }) =>
+    backgroundColor || "var(--main-color)"};
+  color: ${({ color }) => color || "#fff"};
   font-size: ${(props) => props.fontSize};
   font-weight: 700;
   border-radius: 5px;
   margin-top: ${(props) => props.marginTop};
+  margin-left: ${(props) => props.marginLeft};
   text-align: center;
+  border: ${({ border }) => border || "none"};
 `;
