4f17ba5f7a74ba071fa1ef626d317e50a47aa57a
ddhsl <dnjs9199@naver.com>
2025-03-22 04:59:05 +0900

    ✨ 장바구니 목록 가져오기 및 개별 주문하기 기능 추가

--- src/pages/cartPage/component/CartItem.jsx
+++ src/pages/cartPage/component/CartItem.jsx
@@ -3,19 +3,74 @@ import closeIcon from "../../../assets/icon-delete.svg";
 import QuantityControl from "../../productDetails/component/QuantityControl";
 import Button from "../../../components/Button";
 import circleCheck from "../../../assets/circle-check-box-.png";
+import circleCheckOn from "../../../assets/circle-check-box-Fill.png";
 
-export default function CartItem() {
+export default function CartItem({
+  item,
+  onQuantityChange,
+  openDeleteModal,
+  isChecked,
+  onCheck,
+  handleEachOrderClick,
+}) {
+  const { id, product, quantity } = item;
+
+  const handleCartIncrease = () => {
+    const newQuantity = quantity + 1;
+    onQuantityChange(id, newQuantity);
+  };
+
+  const handleCartDecrease = () => {
+    if (quantity > 1) {
+      const newQuantity = quantity - 1;
+      onQuantityChange(id, newQuantity);
+    }
+  };
+
+  const handleCheckClick = () => {
+    onCheck(id);
+  };
+
   return (
-    <CartItemField>
+    <CartItemField key={id}>
+      <img src={closeIcon} alt="닫기" onClick={() => openDeleteModal(item)} />
       <CartItemWrap>
-        <CartItemContent flex={5}>장바구니 아이템</CartItemContent>
-        <CartItemContent flex={2.5}>
-          <QuantityControl />
+        <img
+          src={isChecked ? circleCheckOn : circleCheck}
+          alt="체크 아이콘"
+          onClick={handleCheckClick}
+          style={{ cursor: "pointer" }}
+        />
+        <CartItemContent
+          flex={4.5}
+          style={{ justifyContent: "flex-start", gap: "36px" }}
+        >
+          <img src={product.image} alt="상품사진" />
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              gap: "13px",
+              justifyContent: "center",
+            }}
+          >
+            <p>{product.seller?.store_name}</p>
+            <p>{product.name}</p>
+            <p>{product.price.toLocaleString()}원</p>
+            <p>무료배송 / 택배배송</p>
+          </div>
+        </CartItemContent>
+        <CartItemContent flex={3}>
+          <QuantityControl
+            quantity={quantity}
+            handleDecrease={handleCartDecrease}
+            handleIncrease={handleCartIncrease}
+          />
         </CartItemContent>
         <CartItemContent flex={2.5}>
           <PriceAndOrder>
-            <span>가격</span>
-            <Button>주문하기</Button>
+            <span>{(product.price * quantity).toLocaleString()}원</span>
+            <Button onClick={() => handleEachOrderClick(item)}>주문하기</Button>
           </PriceAndOrder>
         </CartItemContent>
       </CartItemWrap>
@@ -25,32 +80,59 @@ export default function CartItem() {
 
 const CartItemField = styled.article`
   width: 100%;
-  height: 200px;
   border: 2px solid #e0e0e0;
   border-radius: 10px;
-  background-image: url(${closeIcon});
-  background-repeat: no-repeat;
-  background-position: right 10px top 15px;
   display: flex;
-  align-items: center;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 20px 0;
+  margin-bottom: 10px;
+  & > img {
+    position: absolute;
+    right: 105px;
+    cursor: pointer;
+  }
 `;
 
-const CartItemWrap = styled.ul`
+const CartItemWrap = styled.div`
   display: flex;
   align-items: center;
-  width: 100%; /* 자식 항목들이 100% 너비를 차지하도록 */
-  background-image: url(${circleCheck});
-  background-repeat: no-repeat;
-  background-position: left 30px center;
+  width: 100%;
+  padding: 15px 0;
+  & > img {
+    margin-left: 30px;
+    margin-right: 40px;
+  }
 `;
 
-const CartItemContent = styled.li`
+const CartItemContent = styled.div`
   flex: ${({ flex }) => flex};
   display: flex;
   justify-content: center;
+  & > img {
+    width: 160px;
+    height: 160px;
+    border-radius: 10px;
+    object-fit: cover;
+  }
+  & > div > p:nth-child(1) {
+    color: var(--sub-color);
+    font-size: 14px;
+  }
+  & > div > p:nth-child(2) {
+    font-size: 18px;
+  }
+  & > div > p:nth-child(3) {
+    font-weight: bold;
+    margin-bottom: 30px;
+  }
+  & > div > p:nth-child(4) {
+    color: var(--sub-color);
+    font-size: 14px;
+  }
 `;
 
-const PriceAndOrder = styled.li`
+const PriceAndOrder = styled.div`
   display: flex;
   flex-direction: column;
   width: 130px;
