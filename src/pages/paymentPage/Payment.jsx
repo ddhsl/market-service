6e60bf4dfd021efea6675f99ab5d9b437e930c49
ddhsl <dnjs9199@naver.com>
2025-03-22 05:02:05 +0900

    🎨 선택된 장바구니 아이템 정보 가져오기

--- src/pages/paymentPage/Payment.jsx
+++ src/pages/paymentPage/Payment.jsx
@@ -1,30 +1,26 @@
 import styled from "styled-components";
 import Header from "../../components/Header";
-import { CartField } from "../cartPage/Cart";
-import { CartTitle } from "../cartPage/Cart";
-import { CartLabel } from "../cartPage/Cart";
+import { CartField, CartTitle, CartLabel } from "../cartPage/Cart";
 import PaymentItem from "./component/PaymentItem";
 import ShippingInfo from "./component/ShippingInfo";
+import { useLocation } from "react-router-dom";
 
 const paymentLabels = [
-  {
-    text: "상품정보",
-    flex: 4,
-  },
-  {
-    text: "할인",
-    flex: 2,
-  },
-  {
-    text: "배송비",
-    flex: 2,
-  },
-  {
-    text: "주문금액",
-    flex: 2,
-  },
+  { text: "상품정보", flex: 4 },
+  { text: "할인", flex: 2 },
+  { text: "배송비", flex: 2 },
+  { text: "주문금액", flex: 2 },
 ];
+
 export default function Payment() {
+  const location = useLocation();
+  const selectedItems = location.state?.selectedItems || [];
+
+  // 총 금액 계산
+  const totalPrice = selectedItems.reduce((acc, item) => {
+    return acc + item.product?.price * item.quantity;
+  }, 0);
+
   return (
     <>
       <Header />
@@ -37,12 +33,12 @@ export default function Payment() {
             </PaymentLabel>
           ))}
         </PaymentField>
-        <PaymentItem />
+        <PaymentItem selectedItems={selectedItems} />
         <TotalPrice>
           <span>총 주문금액</span>
-          <span>46500원</span>
+          <span>{totalPrice.toLocaleString()}원</span>
         </TotalPrice>
-        <ShippingInfo />
+        <ShippingInfo totalPrice={totalPrice} />
       </PaymentMain>
     </>
   );
