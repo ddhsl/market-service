c616f62c538c0142001ea7b8570baaf74b289671
ddhsl <dnjs9199@naver.com>
2025-03-25 23:00:18 +0900

    🎨 결제방식 선택 로직 추가

--- src/pages/paymentPage/component/PaymentForm.jsx
+++ src/pages/paymentPage/component/PaymentForm.jsx
@@ -1,35 +1,69 @@
 import styled from "styled-components";
 
-export default function PaymentForm() {
+export default function PaymentForm({ paymentMethod, setPaymentMethod }) {
+  const handlePaymentChange = (event) => {
+    setPaymentMethod(event.target.value);
+  };
+
   return (
     <PaymentMethodWrap>
       <PaymentMethodItem>
         <StyledLabel>
-          <input type="radio" name="payment" value="card" />
+          <input
+            type="radio"
+            name="payment"
+            value="card"
+            checked={paymentMethod === "card"}
+            onChange={handlePaymentChange}
+          />
           신용/체크카드
         </StyledLabel>
       </PaymentMethodItem>
       <PaymentMethodItem>
         <StyledLabel>
-          <input type="radio" name="payment" value="bank" />
+          <input
+            type="radio"
+            name="payment"
+            value="deposit"
+            checked={paymentMethod === "deposit"}
+            onChange={handlePaymentChange}
+          />
           무통장 입금
         </StyledLabel>
       </PaymentMethodItem>
       <PaymentMethodItem>
         <StyledLabel>
-          <input type="radio" name="payment" value="mobile" />
+          <input
+            type="radio"
+            name="payment"
+            value="phone"
+            checked={paymentMethod === "phone"}
+            onChange={handlePaymentChange}
+          />
           휴대폰 결제
         </StyledLabel>
       </PaymentMethodItem>
       <PaymentMethodItem>
         <StyledLabel>
-          <input type="radio" name="payment" value="naverpay" />
+          <input
+            type="radio"
+            name="payment"
+            value="naverpay"
+            checked={paymentMethod === "naverpay"}
+            onChange={handlePaymentChange}
+          />
           네이버페이
         </StyledLabel>
       </PaymentMethodItem>
       <PaymentMethodItem>
         <StyledLabel>
-          <input type="radio" name="payment" value="kakaopay" />
+          <input
+            type="radio"
+            name="payment"
+            value="kakaopay"
+            checked={paymentMethod === "kakaopay"}
+            onChange={handlePaymentChange}
+          />
           카카오페이
         </StyledLabel>
       </PaymentMethodItem>
