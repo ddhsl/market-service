e7faa71717dabfad7fb88a9a22f3cd07db356b8a
ddhsl <dnjs9199@naver.com>
2025-03-25 23:02:58 +0900

    🎨 ShippingInput :  props 추가

--- src/pages/paymentPage/component/ShippingInput.jsx
+++ src/pages/paymentPage/component/ShippingInput.jsx
@@ -1,11 +1,19 @@
 import styled from "styled-components";
 
-export default function ShippingInput() {
-  return <StyledShippingInput />;
+export default function ShippingInput({ id, name, value, onChange }) {
+  return (
+    <StyledShippingInput
+      id={id}
+      name={name}
+      value={value}
+      onChange={onChange}
+    />
+  );
 }
 
 export const StyledShippingInput = styled.input`
   width: ${(props) => props.width || "334px"};
   height: 40px;
   border: 1px solid #c4c4c4;
+  padding: 10px;
 `;
