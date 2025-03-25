e7cd9bf034f99c199f1f3dd77175d6a1ef66b7c6
ddhsl <dnjs9199@naver.com>
2025-03-25 23:01:34 +0900

    🎨 PhoneInput : onChange 함수 적용

--- src/pages/paymentPage/component/PhoneInput.jsx
+++ src/pages/paymentPage/component/PhoneInput.jsx
@@ -1,30 +1,35 @@
 import styled from "styled-components";
 import { StyledShippingInput } from "./ShippingInput";
 
-export default function PhoneInput({ idPrefix, namePrefix }) {
+export default function PhoneInput({ idPrefix, namePrefix, value, onChange }) {
   return (
     <div style={{ display: "flex", alignItems: "center" }}>
       <StyledShippingInput
         id={`${idPrefix}-first`}
         name={`${namePrefix}First`}
+        value={value?.first || ""}
+        onChange={(e) => onChange({ ...value, first: e.target.value })}
         width="80px"
       />
       <PhoneSeparator>-</PhoneSeparator>
       <StyledShippingInput
         id={`${idPrefix}-middle`}
         name={`${namePrefix}Middle`}
+        value={value?.middle || ""}
+        onChange={(e) => onChange({ ...value, middle: e.target.value })}
         width="100px"
       />
       <PhoneSeparator>-</PhoneSeparator>
       <StyledShippingInput
         id={`${idPrefix}-last`}
         name={`${namePrefix}Last`}
+        value={value?.last || ""}
+        onChange={(e) => onChange({ ...value, last: e.target.value })}
         width="100px"
       />
     </div>
   );
 }
-
 const PhoneSeparator = styled.span`
   padding: 18px 10px;
 `;
