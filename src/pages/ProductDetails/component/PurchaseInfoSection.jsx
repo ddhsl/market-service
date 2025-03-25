ea8697b2cf67bcf3a9adf23ecc97efe479a7545c
ddhsl <dnjs9199@naver.com>
2025-03-25 23:03:56 +0900

    ✨ 구매정보 swipe 기능 추가

--- src/pages/ProductDetails/component/PurchaseInfoSection.jsx
+++ src/pages/ProductDetails/component/PurchaseInfoSection.jsx
@@ -1,14 +1,33 @@
+import React, { useState } from "react";
 import styled from "styled-components";
+
 export default function PurchaseInfoSection() {
+  const [selectedTab, setSelectedTab] = useState(0);
+
+  const handleTabClick = (index) => {
+    setSelectedTab(index);
+  };
+
   return (
     <section style={{ padding: "0 6%" }}>
       <h2 className="sr-only">구매정보</h2>
       <PurchaseInfoWrap>
         {infoLabel.map((label, index) => (
-          <PurchaseInfoTab key={index}>{label}</PurchaseInfoTab>
+          <PurchaseInfoTab
+            key={index}
+            onClick={() => handleTabClick(index)}
+            isSelected={selectedTab === index}
+          >
+            {label}
+          </PurchaseInfoTab>
         ))}
       </PurchaseInfoWrap>
-      <PurchaseInfo>내용</PurchaseInfo>
+      <PurchaseInfo>
+        {selectedTab === 0 && <TabContent>버튼 내용</TabContent>}
+        {selectedTab === 1 && <TabContent>리뷰 내용</TabContent>}
+        {selectedTab === 2 && <TabContent>Q&A 내용</TabContent>}
+        {selectedTab === 3 && <TabContent>반품/교환정보 내용</TabContent>}
+      </PurchaseInfo>
     </section>
   );
 }
@@ -29,6 +48,15 @@ const PurchaseInfoTab = styled.li`
   display: flex;
   justify-content: center;
   align-items: center;
+  cursor: pointer;
+  transition: all 0.3s ease;
+
+  ${(props) =>
+    props.isSelected &&
+    `
+    border-bottom: 6px solid var(--main-color);
+    color: var(--main-color);
+  `}
 `;
 
 const PurchaseInfo = styled.article`
@@ -38,4 +66,17 @@ const PurchaseInfo = styled.article`
   display: flex;
   justify-content: center;
   align-items: center;
+`;
+
+const TabContent = styled.div`
+  opacity: 0;
+  transform: translateY(20px);
+  animation: fadeIn 0.5s forwards;
+
+  @keyframes fadeIn {
+    to {
+      opacity: 1;
+      transform: translateY(0);
+    }
+  }
 `;
