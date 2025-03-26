5b968be85e2ada549d9c3a7676ca5baed72a7cf5
ddhsl <dnjs9199@naver.com>
2025-03-26 23:32:31 +0900

    🎨 공통 Button 컴포넌트 hover, active 추가

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -7,8 +7,7 @@ export default function Button(props) {
 const StyledButton = styled.button`
   width: ${({ width }) => width || "auto"};
   height: ${({ height }) => height || "auto"};
-  background-color: ${({ backgroundColor }) =>
-    backgroundColor || "var(--main-color)"};
+  background-color: ${({ backgroundColor }) => backgroundColor || "#28d358"};
   color: ${({ color }) => color || "#fff"};
   font-size: ${(props) => props.fontSize};
   font-weight: 700;
@@ -17,4 +16,28 @@ const StyledButton = styled.button`
   margin-left: ${(props) => props.marginLeft};
   text-align: center;
   border: ${({ border }) => border || "none"};
+  transition: background-color 0.2s ease-in-out;
+  cursor: ${({ disabled }) => (disabled ? "not-allowed" : "pointer")};
+
+  &:hover {
+    background-color: ${({ buttonType, disabled }) =>
+      disabled
+        ? ""
+        : buttonType === "cart"
+        ? "var(--sub-color)"
+        : buttonType === "cancel"
+        ? "#f2f2f2"
+        : "#21bf48"};
+  }
+
+  &:active {
+    background-color: ${({ buttonType, disabled }) =>
+      disabled
+        ? ""
+        : buttonType === "cart"
+        ? "#5c5c5c"
+        : buttonType === "cancel"
+        ? "#e0e0e0"
+        : "#1aa03d"};
+  }
 `;
