d945035f09ca5c556a0cdcaa146fb239eefbfce2
ddhsl <dnjs9199@naver.com>
2025-03-26 23:41:05 +0900

    ⚡️ OrderProvider 전역적 적용 (index.js에 추가)

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthContextProvider } from "./context/AuthContext";
+import { OrderProvider } from "./context/OrderContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <AuthContextProvider>
-      <App />
+      <OrderProvider>
+        <App />
+      </OrderProvider>
     </AuthContextProvider>
   </React.StrictMode>
 );
