8b4b1bd60e0f208d826b45e5d328e01d339d3ee4
ddhsl <dnjs9199@naver.com>
2025-03-27 21:06:52 +0900

    basename에 홈페이지 경로 추가

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ function App() {
   }
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/market-service">
       <GlobalStyle />
       <Routes>
         <Route path="/" element={<Home />} />
