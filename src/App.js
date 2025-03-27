cf73133b9b708cf7c98e0d9b9a9fbbe6d1995a7d
ddhsl <dnjs9199@naver.com>
2025-03-27 19:18:27 +0900

    ✨ 로그인 여부 / 회원유형에 따른 라우터 보호 작업

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import GlobalStyle from "./styles/GlobalStyle";
 import Home from "./pages/Home.jsx";
 import Login from "./pages/login/Login.jsx";
@@ -11,8 +11,33 @@ import RegisterProduct from "./pages/registerProduct/RegisterProduct.jsx";
 import ErrorPage from "./pages/error/ErrorPage.jsx";
 import MyOrderPage from "./pages/myPage/MyOrderPage.jsx";
 import SearchResults from "./pages/search/SearchResults.jsx";
+import { useAuth } from "./context/AuthContext";
+import { useEffect, useState } from "react";
+import Loader from "./components/Loader.jsx";
 
 function App() {
+  const { isLoggedIn, isSeller, refreshAccessToken } = useAuth();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const checkLoginStatus = async () => {
+      if (isLoggedIn) {
+        const newAccessToken = await refreshAccessToken();
+        if (!newAccessToken) {
+          setLoading(false);
+        }
+      } else {
+        setLoading(false);
+      }
+    };
+
+    checkLoginStatus();
+  }, [isLoggedIn, refreshAccessToken]);
+
+  if (loading) {
+    return <Loader />;
+  }
+
   return (
     <BrowserRouter>
       <GlobalStyle />
@@ -21,16 +46,54 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/join" element={<Join />} />
         <Route path="/product-details/:id" element={<ProductDetails />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/payment" element={<Payment />} />
-        <Route path="/seller-center" element={<SellerCenter />} />
+
+        {/* 구매회원만 접근 가능한 페이지 */}
+        <Route
+          path="/cart"
+          element={
+            isLoggedIn && !isSeller ? <Cart /> : <Navigate to="/login" />
+          }
+        />
+        <Route
+          path="/payment"
+          element={
+            isLoggedIn && !isSeller ? <Payment /> : <Navigate to="/login" />
+          }
+        />
+        <Route
+          path="/mypage"
+          element={
+            isLoggedIn && !isSeller ? <MyOrderPage /> : <Navigate to="/login" />
+          }
+        />
+
+        {/* 판매회원만 접근 가능한 페이지 */}
+        <Route
+          path="/seller-center"
+          element={
+            isLoggedIn && isSeller ? <SellerCenter /> : <Navigate to="/login" />
+          }
+        />
         <Route
           path="/register"
-          element={<RegisterProduct isEditMode={false} />}
+          element={
+            isLoggedIn && isSeller ? (
+              <RegisterProduct isEditMode={false} />
+            ) : (
+              <Navigate to="/login" />
+            )
+          }
         />
-        <Route path="/register/:id" element={<RegisterProduct />} />
-
-        <Route path="/mypage" element={<MyOrderPage />} />
+        <Route
+          path="/register/:id"
+          element={
+            isLoggedIn && isSeller ? (
+              <RegisterProduct />
+            ) : (
+              <Navigate to="/login" />
+            )
+          }
+        />
         <Route path="/search" element={<SearchResults />} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
